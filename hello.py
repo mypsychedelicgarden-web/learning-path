5e941608d137f2a4d6fa38a954d4bbe2b1631cb6
Inna Etuvgi <mypsychedelicgarden@gmail.com>
2026-08-21 21:35:06 +0200

    добавила переменные artwork

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
 print("Hej! Hello from My Psychedelic Garden!")
+artwork_title = "Self-Reflection"
+artwork_edition_number = 25
+artwork_price = 600.60
+print(artwork_title, "edition", artwork_edition_number, "price", artwork_price)
