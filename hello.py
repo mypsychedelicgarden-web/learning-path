4bcf9f26f1fff82abdad1699708786129e4d0234
Inna Etuvgi <mypsychedelicgarden@gmail.com>
2026-08-21 23:45:23 +0200

    added 2f

--- hello.py
+++ hello.py
@@ -4,6 +4,6 @@ artwork_title = "Self-Reflection"
 artwork_edition_number = 25
 artwork_price = 600.60
 print(artwork_title, "edition", artwork_edition_number, "price", artwork_price)
-print(f"Fine art print by Inna Etuvgi '{artwork_title}' is printed in edition of {artwork_edition_number} and is priced at {artwork_price} EUR.")
+print(f"Fine art print by Inna Etuvgi '{artwork_title}' is printed in edition of {artwork_edition_number} and is priced at {artwork_price:.2f} EUR.")
 #вводим операции с переменными - и выводим, например предложение цены со скидкой 10% на печать
 print(f"the artwork price with 10% discount is {artwork_price * 0.9:.2f} EUR.")
