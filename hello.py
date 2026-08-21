8494944d6549fdc7653a1c25af4637ed41916475
Inna Etuvgi <mypsychedelicgarden@gmail.com>
2026-08-21 21:50:50 +0200

    new print F line

--- hello.py
+++ hello.py
@@ -4,3 +4,4 @@ artwork_title = "Self-Reflection"
 artwork_edition_number = 25
 artwork_price = 600.60
 print(artwork_title, "edition", artwork_edition_number, "price", artwork_price)
+print(f"Fine art print by Inna Etuvgi '{artwork_title}' is printed in edition of {artwork_edition_number} and is priced at {artwork_price} EUR.")
