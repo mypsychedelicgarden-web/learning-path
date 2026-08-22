acd8974f37c48e24859c1f95c89ae854b8f89919
Inna Etuvgi <mypsychedelicgarden@gmail.com>
2026-08-22 03:15:22 +0200

    added lists and ZIP

--- hello.py
+++ hello.py
@@ -7,3 +7,8 @@ print(artwork_title, "edition", artwork_edition_number, "price", artwork_price)
 print(f"Fine art print by Inna Etuvgi '{artwork_title}' is printed in edition of {artwork_edition_number} and is priced at {artwork_price:.2f} EUR.")
 #вводим операции с переменными - и выводим, например предложение цены со скидкой 10% на печать
 print(f"the artwork price with 10% discount is {artwork_price * 0.9:.2f} EUR.")
+collections = ["ALIEN", "InnerSpace", "Wonderland"]
+works = [12, 8, 15]
+editions = 25
+for title, count in zip(collections, works):
+print(f"Collection {title} contains {count*editions} prints")
